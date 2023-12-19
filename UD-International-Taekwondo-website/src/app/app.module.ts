1906578a8143c40c7aa410a8041d42e05bae45b6
Daniel-Frederick <frederickd5@udayton.edu>
2023-12-19 01:29:45 -0500

    added a membership component

--- UD-International-Taekwondo-website/src/app/app.module.ts
+++ UD-International-Taekwondo-website/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MainPageComponent } from "./components/main-page/main-page.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { MembershipComponent } from './components/membership/membership.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { FooterComponent } from "./components/footer/footer.component";
     MainPageComponent,
     HeaderComponent,
     FooterComponent,
+    MembershipComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
